db7518dd8c07657452330b01bc55cab746f73b54
Daniel Kudwien <daniel@netzstrategen.com>
2021-07-07 12:39:35 +0200

    Fixed Auth and Devices class methods cannot be accessed from integration plugins.

--- class-setup.php
+++ class-setup.php
@@ -11,19 +11,34 @@ namespace JWTAuth;
  * Setup JWT Auth.
  */
 class Setup {
+
+	private static $instance;
+	public $auth;
+	public $devices;
+
+	/**
+	 * Constructs singleton.
+	 */
+	public static function getInstance() {
+		if (!isset(self::$instance)) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
 	/**
 	 * Setup action & filter hooks.
 	 */
 	public function __construct() {
 		add_action( 'init', array( $this, 'setup_text_domain' ) );
 
-		$auth    = new Auth();
-		$devices = new Devices();
+		$this->auth    = new Auth();
+		$this->devices = new Devices();
 
-		add_action( 'rest_api_init', array( $auth, 'register_rest_routes' ) );
-		add_filter( 'rest_api_init', array( $auth, 'add_cors_support' ) );
-		add_filter( 'rest_pre_dispatch', array( $auth, 'rest_pre_dispatch' ), 10, 3 );
-		add_filter( 'determine_current_user', array( $auth, 'determine_current_user' ) );
+		add_action( 'rest_api_init', array( $this->auth, 'register_rest_routes' ) );
+		add_filter( 'rest_api_init', array( $this->auth, 'add_cors_support' ) );
+		add_filter( 'rest_pre_dispatch', array( $this->auth, 'rest_pre_dispatch' ), 10, 3 );
+		add_filter( 'determine_current_user', array( $this->auth, 'determine_current_user' ) );
 	}
 
 	/**
