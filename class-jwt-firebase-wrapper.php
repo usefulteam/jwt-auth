f7111e5da591c8c703f85d86cb1556aaebd92f3c
Michael Sowah <MichaelSowah@users.noreply.github.com>
2022-12-29 18:07:19 +0000

    Added a wrapper class around \Firebase\JWT\JWT and \Firebase\JWT\Key
    Added a wrapper class around \Firebase\JWT\JWT and \Firebase\JWT\Key to help avoid plugin conflicts such as https://github.com/usefulteam/jwt-auth/issues/82

---
+++ class-jwt-firebase-wrapper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Setup class that wraps JWT-Auth Firebase class.
+ *
+ * @package jwt-auth
+ */
+
+namespace JWTWrapAuth\Firebase\JWT;
+
+/**
+ * A class to extend \Firebase\JWT\JWT.
+ */
+class JWT extends \Firebase\JWT\JWT { }
+
+/**
+ * A class to extend \Firebase\JWT\Key.
+ */
+class Key extends \Firebase\JWT\Key { }
