d3a3e8f6d2b476c5c22601c5dd1d9d05138b59d3
bradmkjr <brad@ucanbonline.net>
2023-04-12 03:47:31 -0600

    Do not validate authentication headers if a valid user was determined already. (#75)
    Co-authored-by: Daniel Kudwien <daniel@netzstrategen.com>

--- class-auth.php
+++ class-auth.php
@@ -607,7 +607,8 @@ class Auth {
 
 		$valid_api_uri = strpos( $_SERVER['REQUEST_URI'], $this->rest_api_slug );
 
-		if ( ! $valid_api_uri ) {
+		// Skip validation if not a REST API request or a user was determined already.
+		if ( ! $valid_api_uri || $user_id ) {
 			return $user_id;
 		}
 
